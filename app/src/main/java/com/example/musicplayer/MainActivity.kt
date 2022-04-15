4fa14b3cd446af82160368636569121f27e0846b
VictorJanampa <torrviccc@gmail.com>
2022-04-15 12:34:38 -0500

    feat: added main and details layouts

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.musicplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.overflow_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.song_detail_menu) {
+            startActivity(Intent(this,DetailsActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
