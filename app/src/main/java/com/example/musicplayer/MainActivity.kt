4b84ed5f15362a55218f776fbeb72b006fab7997
VictorJanampa <torrviccc@gmail.com>
2022-04-15 21:31:23 -0500

    feat: added skip to previous and skip to next functionalities

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -11,6 +11,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.databinding.DataBindingUtil
@@ -23,47 +24,68 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var songDetails: Song
     private lateinit var mediaMetadataRetriever: MediaMetadataRetriever
+    private lateinit var mediaPlayer: MediaPlayer
+    private var currentSong: Int = 0
+    private var playlistSize: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val songList = listOf(R.raw.dreaming)
-        songDetails = retrieveMetadata(songList[0])
+        val songList = listOf(R.raw.hard_in_da_paint, R.raw.dreaming, R.raw.dreamer).also { playlistSize = it.size }
+        songDetails = retrieveMetadata(songList[currentSong])
 
         binding.songDetails = songDetails
         binding.albumCoverImageView.setImageDrawable(songDetails.cover)
 
-        val mediaPlayer = MediaPlayer.create(this, songList[0])
+        mediaPlayer = MediaPlayer.create(this, songList[currentSong])
 
         binding.playPauseButton.setOnClickListener {
-            if (!mediaPlayer.isPlaying) {
-                mediaPlayer.start()
-                it.background = AppCompatResources.getDrawable(this, R.drawable.ic_baseline_pause_24)
-            }else{
-                mediaPlayer.pause()
-                it.background = AppCompatResources.getDrawable(this, R.drawable.ic_baseline_play_arrow_24)
-            }
+            if (!mediaPlayer.isPlaying) playMusic() else pauseMusic()
         }
         binding.skipNextButton.setOnClickListener {
-
+            currentSong = (currentSong+1) % songList.size
+            mediaPlayer.release()
+            mediaPlayer = MediaPlayer.create(this, songList[currentSong]).apply {  }
+            playMusic()
+            updateUI(songList)
         }
         binding.skipPreviousButton.setOnClickListener {
-            mediaPlayer.stop()
-            mediaPlayer.prepare()
-            mediaPlayer.start()
-            binding.playPauseButton.background = AppCompatResources.getDrawable(this, R.drawable.ic_baseline_pause_24)
+            currentSong--
+            if (currentSong < 0) currentSong = playlistSize - 1
+            mediaPlayer.release()
+            mediaPlayer = MediaPlayer.create(this, songList[currentSong])
+            updateUI(songList)
+            playMusic()
         }
-
+    }
+    // functions to control the player
+    private fun playMusic() {
+        mediaPlayer.start()
+        binding.playPauseButton.background = AppCompatResources.getDrawable(this, R.drawable.ic_baseline_pause_24)
     }
 
-        private fun retrieveMetadata(resourceId: Int): Song {
+    private fun pauseMusic() {
+        mediaPlayer.pause()
+        binding.playPauseButton.background = AppCompatResources.getDrawable(this, R.drawable.ic_baseline_play_arrow_24)
+    }
+
+    // Update UI
+
+    private fun updateUI(songList: List<Int>) {
+        songDetails = retrieveMetadata(songList[currentSong])
+        binding.albumCoverImageView.setImageDrawable(songDetails.cover)
+    }
+
+    // Get Metadata
+    private fun retrieveMetadata(resourceId: Int): Song {
         mediaMetadataRetriever = MediaMetadataRetriever()
+
         val uri = Uri.parse("android.resource://${this.packageName}/$resourceId")
 
         mediaMetadataRetriever.setDataSource(this, uri)
-        mediaMetadataRetriever.run {
-             return Song(
+        val song = mediaMetadataRetriever.run {
+             Song(
                 extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE).toString(),
                 extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST).toString(),
                 extractMetadata(MediaMetadataRetriever.METADATA_KEY_YEAR).toString(),
@@ -72,8 +94,10 @@ class MainActivity : AppCompatActivity() {
                 embeddedPicture.toDrawable() ?: AppCompatResources.getDrawable(baseContext, R.drawable.albumart)
             )
         }
+        return song
     }
 
+    // Options Menu
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.overflow_menu,menu)
         return super.onCreateOptionsMenu(menu)
@@ -87,6 +111,7 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+// Extension to convert a ByteArray to Drawable
 private fun ByteArray?.toDrawable(): Drawable? {
     return this?.let {
         BitmapDrawable(Resources.getSystem() ,BitmapFactory.decodeByteArray(this, 0, it.size))
